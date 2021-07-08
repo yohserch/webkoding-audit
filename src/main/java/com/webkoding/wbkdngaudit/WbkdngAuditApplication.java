988304d04581f9a15ef63fa7449b3c5660ca12b2
Serch Durán <def.serch@gmail.com>
2021-07-08 01:29:23 -0500

    Initial commit

---
+++ src/main/java/com/webkoding/wbkdngaudit/WbkdngAuditApplication.java
@@ -0,0 +1,13 @@
+package com.webkoding.wbkdngaudit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WbkdngAuditApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WbkdngAuditApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/webkoding/wbkdngaudit/WbkdngAuditApplicationTests.java
@@ -0,0 +1,13 @@
+package com.webkoding.wbkdngaudit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WbkdngAuditApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
